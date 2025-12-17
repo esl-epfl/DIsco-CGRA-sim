d77bc8e532bbd79bdf8c0c9855ac744995d6e002
mbelda <11842513+mbelda@users.noreply.github.com>
2025-12-17 12:46:00 +0100

    fix signed issue

--- src/alu.py
+++ src/alu.py
@@ -31,16 +31,9 @@ class ALU():
         self.newRes = c_int32(val1 << shift_n).value
 
     def srl(self,  val1, val2 ):
-        # Python shifts are always arithmetic so...
-        mask = (1 << 3) - 1 # Get the three last bits of val2 for the shift
+        mask = 0b111
         shift_n = val2 & mask
-        if val1 >= 0:
-            interm_result = (c_int32(val1).value & MAX_32b)
-            self.newRes = c_int32(interm_result >> shift_n).value
-        else:
-            res_pos = c_int32((-val1) >> shift_n).value
-            self.newRes = - res_pos
-        
+        self.newRes = (val1 % (1 << 32)) >> shift_n
 
     def sra(self,  val1, val2 ):
         mask = (1 << 3) - 1 # Get the three last bits of val2 for the shift
@@ -57,10 +50,10 @@ class ALU():
         self.newRes = c_int32( val1 ^ val2).value
     
     def smul(self,  val1, val2):
-        self.newRes = c_int32(val1 * val2).value & MAX_32b
+        self.newRes = c_int32(val1 * val2).value 
 
     def sdiv(self,  val1, val2):
-        self.newRes = c_int32(val1 / val2).value
+        self.newRes = c_int32(val1 // val2).value
     
     def saddh(self,  val1, val2 ):
         val1_high = (val1 >> 16) & 0xFFFF  
@@ -147,7 +140,7 @@ class ALU():
         self.newRes = c_int32(interm_result >> shift_n).value
     
     def mac(self, val1, val2, val3):
-        self.newRes = c_int32(val1 * val2).value + val3
+        self.newRes = c_int32(c_int32(val1 * val2).value + val3).value
         print(f"{val1} * {val2} + {val3}") # DEBUG
 
     def mach(self, val1, val2, val3):
